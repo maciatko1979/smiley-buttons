5af15d413c713438e29e668ae950dce27ae42008
maciatko1979 <99343414+maciatko1979@users.noreply.github.com>
2022-02-09 16:58:41 +0100

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.showString("Happy")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Asleep)
+    basic.showString("I dont know")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+    basic.showString("Sad")
+})
